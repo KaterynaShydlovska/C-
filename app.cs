eff87da5ee2e692e1d4cca7eee831cb6a5efdfaf
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-12-12 15:41:03 -0600

    Can we divide it?

--- app.cs
+++ app.cs
@@ -77,3 +77,20 @@ public class Kata
   }
 }
 
+// Your task is to create functionisDivideBy (or is_divide_by) to check if an integer number is divisible by each out of two arguments.
+// A few cases:
+
+// (-12, 2, -6)  ->  true
+// (-12, 2, -5)  ->  false
+
+public class Kata
+{
+    public static bool is_divide_by(int number, int a, int b)
+    {
+        if(number % a == 0 && number % b == 0)
+          {
+          return true;
+        }
+          return false;
+    }
+}
