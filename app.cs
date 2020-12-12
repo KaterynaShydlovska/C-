5bb96f6b86d6ee1cc5b558e7b481fba58101e142
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-12-12 15:32:15 -0600

    Filling an array

--- app.cs
+++ app.cs
@@ -52,3 +52,28 @@ public static class Kata
   }
 }
 
+// We want an array, but not just any old array, an array with contents!
+
+// Write a function that produces an array with the numbers 0 to N-1 in it.
+
+// For example, the following code will result in an array containing the numbers 0 to 4:
+
+// arr(5) // => [0,1,2,3,4]
+
+public class Kata
+{
+  public static int[] Arr(int N)
+    
+  {
+    int[] array = new int[N];
+    if(N == 0)
+      return array;
+    for(var i=array.Length -1 ; i>=0; i--)
+      {
+      N--;
+      array[i] =N ;
+    }
+    return array;
+  }
+}
+
