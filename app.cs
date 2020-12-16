2687f96e472ec0609dea4737ecf0e4c89300bf6d
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-12-15 21:51:50 -0600

    Max-min arrays

--- app.cs
+++ app.cs
@@ -742,8 +742,45 @@ public class Drinkin
       }
       
 
+// In this Kata, you will be given an array of unique elements, and your task is to rerrange the values so that the first max value is followed by the first minimum, followed by second max value then second min value, etc.
+
+// For example:
+
+// Kata.Solve(new List<int> {15,11,10,7,12}) => new List<int> {15,7,12,10,11}
+// The first max is 15 and the first min is 7. The second max is 12 and the second min is 10 and so on.
 
 
+using System;
+using System.Collections.Generic;
+
+public static class Kata
+{
+  public static List<int> Solve(List<int> arr)
+  {
+    arr.Sort();
+    List<int> result = new List<int>();
+    
+    int i=0;
+    int j = arr.Count-1;
+    
+    
+    while(i <=j)
+      {
+      if(i == j ){
+        result.Add(arr[i]);
+      }
+      else{
+        result.Add(arr[j]);
+      result.Add(arr[i]);
+      }
+      j--;
+      i++;
+    }
+    
+    return result;
+    
+  }
+}
 
 
 
