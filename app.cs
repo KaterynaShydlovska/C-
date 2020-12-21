c24f6b66cf9d3fa39129d67cb9da54e318c72cd8
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-12-20 18:53:27 -0600

    Sort Numbers

--- app.cs
+++ app.cs
@@ -831,6 +831,33 @@ public class Kata
   }
 }
 
+// Sort Numbers
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// SortNumbers(new int[] { 1, 2, 10, 50, 5 }); // should return new int[] { 1, 2, 5, 10, 50 }
+// SortNumbers(null); // should return new int[] { }
+
+using System;
+using System.Collections.Generic;
+public class Kata
+{
+  public static int[] SortNumbers(int[] nums)
+  {
+    if(nums == null || nums.Length == 0 )
+      {
+       return new int[0];
+    }
+  else
+  {
+    Array.Sort(nums);
+   }
+    return nums;
+  }
+}
+
 
 
 
