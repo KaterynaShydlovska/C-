3c99c9bbc4a1bc94f220d448ae653e929008b7c1
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-12-22 20:30:02 -0600

    added comments

--- pre-work.cs
+++ pre-work.cs
@@ -167,3 +167,8 @@ Console.WriteLine($"RESULT: {result}");
 }
 
 /////////////////////
+//command-line application using character escape sequences
+Console.WriteLine("Generating invoices for customer \"ABC Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
