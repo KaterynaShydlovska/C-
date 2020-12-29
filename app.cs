3299118dbab57ebcc9344fe3d4995fcfe8ad8a29
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-12-29 16:42:18 -0600

    Take the Derivative

--- app.cs
+++ app.cs
@@ -1178,6 +1178,27 @@ public static class Kata
         return new string(buff);
     }
  
+//  This function takes two numbers as parameters, the first number being the coefficient, and the second number being the exponent.
+
+// Your function should multiply the two numbers, and then subtract 1 from the exponent. Then, it has to print out an expression (like 28x^7). "^1" should not be truncated when exponent = 2.
+
+// For example:
+
+// derive(7, 8)
+// In this case, the function should multiply 7 and 8, and then subtract 1 from 8. It should output "56x^7", the first number 56 being the product of the two numbers, and the second number being the exponent minus 1.
+
+// derive(7, 8) --> this should output "56x^7" 
+// derive(5, 9) --> this should output "45x^8" 
+
+public class Kata
+{
+  public static string Derive(double coefficient, double exponent)
+  {
+    double res = coefficient * exponent;
+    return $"{res}x^{exponent-1}";
+    
+  }
+}
 
 
 
