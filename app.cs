c5efee07ffcda99b6bc4f945f8e6dc93364aa53b
Kateryna Shydlovska <shydlovskayaka@gmail.com>
2020-12-30 18:25:45 -0600

    Vowel Count

--- app.cs
+++ app.cs
@@ -1418,6 +1418,33 @@ namespace Calculator
     }
 }
 
+// Return the number (count) of vowels in the given string.
+
+// We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+// The input string will only consist of lower case letters and/or spaces.
+
+using System;
+using System.Linq;
+
+public static class Kata
+{
+    public static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+
+        for(var i=0; i< str.Length; i++)
+          {
+          if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
+            {
+            vowelCount++;
+          }
+        }
+
+        return vowelCount;
+    }
+}
+
 
 
 
